1857eae5d26b6df0c2b10040876ef273a962c66c
Keyem04 <krstnmvrgs04@gmail.com>
2026-02-09 12:42:58 +0800

    for print2

--- resources/views/filament/print.blade.php
+++ resources/views/filament/print.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full h-full">
     <iframe 
-        src="http://reports.dvodeoro.local:8080/jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA,Sales%7Cpa1%3DSweden&_flowId=viewReportFlow&ParentFolderUri=%2Freports%2Faccomplishments&reportUnit=%2Freports%2Faccomplishments%2Faccomplishment&standAlone=true&decorate=no&output=pdf&department_id={{ $department_id }}&reporting_year={{ $year }}&reporting_month={{ $month }}" 
+        src="http://reports.dvodeoro.local:8080/jasperserver/flow.html?pp=u%3DJamshasadid%7Cr%3DManager%7Co%3DEMEA,Sales%7Cpa1%3DSweden&_flowId=viewReportFlow&ParentFolderUri=%2Freports%2Faccomplishments&reportUnit=%2Freports%2Faccomplishments%2Faccomplishment&standAlone=true&decorate=no&department_id={{ $department_id }}&reporting_year={{ $year }}&reporting_month={{ $month }}&output=pdf" 
         frameborder="0"
         width="100%" 
         height="800px">
