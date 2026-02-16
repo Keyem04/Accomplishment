8bfa595e487cc9463e8f73b5726b21fcc1127272
Keyem04 <krstnmvrgs04@gmail.com>
2026-02-16 14:34:43 +0800

    add preparedby in printcontroller

--- app/Http/Controllers/Api/AccomplishmentPrintController.php
+++ app/Http/Controllers/Api/AccomplishmentPrintController.php
@@ -77,7 +77,7 @@ class AccomplishmentPrintController extends Controller
                     'image1' => $images->get(0), // null if not exists
                     'image2' => $images->get(1), // null if not exists
                     'include_in_print' => $item->include_in_print,
-                    'user_name' => trim($preparedBy), 
+                    'prepared_by' => trim($preparedBy), 
                 ];
 
                 return $data;
